018dfd456fdddb42ba16a2f24a43fc2e441e3a53
Illia Solonetskyi <mr.king101@ukr.net>
2025-03-26 20:56:05 +0200

    fix/index.ts and App.tsx include router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./Pages/Home";
 import { Email } from "./Pages/Email";
@@ -7,13 +7,11 @@ import { Other } from "./Pages/Other";
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/email" element={<Email />} />
-        <Route path="/tests" element={<Tests />} />
-        <Route path="/other" element={<Other />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/email" element={<Email />} />
+      <Route path="/tests" element={<Tests />} />
+      <Route path="/other" element={<Other />} />
+    </Routes>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import "./style/global/_normalization.scss";
-
 import { App } from "./App";
 
 const root = ReactDOM.createRoot(
@@ -10,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/quarks-test-task">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
